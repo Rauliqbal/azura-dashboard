c0f4e57d8ee71003e115cdabd47d9b72b496cf0a
Rauliqbal <muhamadrauliqbal.gt@gmail.com>
2024-07-16 14:51:41 +0700

    [FIX] layout table

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -3,24 +3,24 @@ import Header from "@/components/Dashboard/Header.vue";
 import Sidebar from "@/components/Dashboard/Sidebar.vue";
 </script>
 <template>
-    <div class="flex h-screen overflow-hidden bg-gray-100">
-        <!-- Sidebar -->
-        <Sidebar />
+  <div class="flex h-screen overflow-hidden bg-gray-100">
+    <!-- Sidebar -->
+    <Sidebar />
 
-        <!-- Content Navbar -->
-        <div
-            class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden"
-        >
-            <div class="bg-primary w-full h-96 absolute top-0 left-0"></div>
-            <!-- Header -->
-            <Header />
+    <!-- Content Navbar -->
+    <div
+      class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden"
+    >
+      <div class="bg-primary w-full h-96 absolute top-0 left-0"></div>
+      <!-- Header -->
+      <Header />
 
-            <!-- Body -->
-            <main class="z-10">
-                <div class="mx-auto max-w-screen-2xl p-4 lg:p-6">
-                    <slot />
-                </div>
-            </main>
+      <!-- Body -->
+      <main class="z-10">
+        <div class="mx-auto max-w-screen-2xl p-4 lg:px-10">
+          <slot />
         </div>
+      </main>
     </div>
+  </div>
 </template>
