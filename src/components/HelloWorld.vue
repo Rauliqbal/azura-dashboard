8cf243c2e837a31f5d87dec215ab6578b35e1e47
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-02-01 11:46:26 +0700

    style: added font poppins

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <script setup></script>
 <template>
+   <header class="py-6 bg-gray-100">
+      <img class="w-14 mx-auto" src="../assets/logo.svg" alt="" />
+   </header>
    <div class="container">
-      <h1>Hello World</h1>
+      <h1>Hello Worlda</h1>
    </div>
 </template>
